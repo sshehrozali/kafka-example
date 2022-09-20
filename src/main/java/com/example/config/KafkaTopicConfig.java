85d84e8718f6a6a068b91038e021262501ec2e14
Shehroz Ali <sshehroza13@gmail.com>
2022-09-20 12:43:56 +0500

    kafkaTopic(): returned TopicBuilder.name().builder kafkaTopic:() Annoated method with @Bean

--- src/main/java/com/example/config/KafkaTopicConfig.java
+++ src/main/java/com/example/config/KafkaTopicConfig.java
@@ -1,4 +1,12 @@
 package com.example.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+
 public class KafkaTopicConfig {
+    @Bean
+    public NewTopic kafkaTopic() {
+        return TopicBuilder.name("Kafka Topic").build();
+    }
 }
