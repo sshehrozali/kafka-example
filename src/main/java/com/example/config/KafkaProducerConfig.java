afc920e0d8a3a8fc07ef2701d6c362bdea83f916
Shehroz Ali <sshehroza13@gmail.com>
2022-09-21 12:52:17 +0500

    KafkaProducerConfig: Created private String bootstrapServers KafkaProducerConfig: Annotated with @Value passing spring.kafka.bootstrap-servers value

--- src/main/java/com/example/config/KafkaProducerConfig.java
+++ src/main/java/com/example/config/KafkaProducerConfig.java
@@ -1,7 +1,10 @@
 package com.example.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KafkaProducerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 }
