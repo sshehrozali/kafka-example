6889bdacde80afb5352a784e03f6094d1d2acbdf
Shehroz Ali <sshehroza13@gmail.com>
2022-09-22 13:59:11 +0500

    listner(): Displayed consumed Kafka events from topic listner(): Annotated the class with @KafkaListner(topics = "topic", groupId = "groupdId")

--- src/main/java/com/example/KafkaListeners.java
+++ src/main/java/com/example/KafkaListeners.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaListeners {
+    @KafkaListener(topics = "topic", groupId = "groupId")
+    void listener(String data) {
+        System.out.println("Received: " + data);
+    }
 }
