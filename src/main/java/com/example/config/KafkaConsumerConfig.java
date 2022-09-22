fb68dc1f2e14df11f8ebb40ad422c32a73c897bc
Shehroz Ali <sshehroza13@gmail.com>
2022-09-22 13:36:08 +0500

    KafkaConsumerConfig: Created new private String bootstrapServers annotated with @Value("localhost:9092") KafkaConsumerConfig: Implemented consumerConfig() method

--- src/main/java/com/example/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/config/KafkaConsumerConfig.java
@@ -1,4 +1,21 @@
 package com.example.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    public Map<String, Object> consumerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
 }
