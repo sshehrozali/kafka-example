36cf411a17312b1abb7d5e6ee8c6967c6574304b
Shehroz Ali <sshehroza13@gmail.com>
2022-09-22 13:41:20 +0500

    KafkaConsumerConfig: Implemented consumerFactory() method

--- src/main/java/com/example/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/config/KafkaConsumerConfig.java
@@ -3,6 +3,11 @@ package com.example.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,5 +22,11 @@ public class KafkaConsumerConfig {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
+    }
+
+    // Responsible for handling Producer messages
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
     }
 }
